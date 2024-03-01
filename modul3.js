2f77d6268f82f96981a37e0bc4b32037c8a7b178
kajalpreetinag <161806327+kajalpreetinag@users.noreply.github.com>
2024-03-01 14:13:36 +0530

    Add files via upload

---
+++ modul3.js
@@ -0,0 +1,10 @@
+const input = require("readline-sync")
+let a = input.questionInt("enter date : ")
+let b = input.questionInt("enter month: ")
+let c = input.questionInt("enter year: ")
+
+{
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
